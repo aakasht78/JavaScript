eb5f4e120e38986dc187f10677163d2079849ad7
Aakash <aakashtiwari605@gmail.com>
2022-11-04 20:55:30 +0530

    more code added

--- JavaScripts/Array.js
+++ JavaScripts/Array.js
@@ -98,6 +98,114 @@
     const js1 = js.sort()
     console.log(js1)
 
+//!======================================04/11/22===============================
+
+    //!  Array Method Using Functions
+    //*For of :-
+    //?     This method iterates each element one by one which is present in an array in the form if Strings
+
+    let a11 =["hi","hello","good", "evening"]
+    for(const b of a11){
+        console.log(b)
+    }
 
 
-    
+    //!  For IN :-
+//?         This Methods iterates or returns the index value one by one from an array
+
+        let x1 = ["a","b","c","d","e"]
+
+        for(let y in x1){
+            console.log(y)
+        }
+
+console.log("===============================================================")
+        //! For Each method:-
+        //?      This method it will iterate elements in an array abd it will also iterates index values from an array    
+
+
+        let students = ["ambika","amar","shubham","pavan", "pooja"]
+        students.forEach(iteratingbot)
+        function iteratingbot(element,index){
+            console.log(element)
+            console.log(index)
+        }
+
+
+
+        //! Map () :-
+//?             This method will create new Array from calling a function for every array element .
+//?             It will transform the whole array but it will return the same lenghth of ana array
+//?
+        const arr=[5,1,3,2,6]
+        function double (x){
+            return x*2
+        }
+
+        const result = arr.map(double)
+        console.log(result)
+        console.log(arr)
+        
+
+        const m = [4,3,2,1,5]
+        let n = m.map((a)=>{
+            return a*2
+        })
+        // console.log(n)
+
+
+        //! Filter Method:-
+        //?        This method will filter an element in an array based on condition given inside the function
+
+
+        const age = [30,18,35,16,19,24]
+        function adult (checkadult){
+            return checkadult >=18;
+        }
+        let newage=age.filter(adult)
+        console.log(newage)
+
+
+        //! Reduce Method :-
+        //?         it will reduce the elements in an array and return single output
+
+
+        const red = ["very", "good","evening"]
+        function join(a,b) {
+            return a+b
+        }
+        const red1 =red.reduce(join)
+        console.log(red1)
+
+        let add=[10,20,30,40,50]
+        function adding(a,b){
+            return a+b
+        }
+
+        let add12=add.reduce(adding)
+        console.log(add12)
+
+        const values = [1,2,5,32,1,32,6,323,65,34,326]
+        let largest = values.reduce((l1,l2)=>{
+            return l1>l2? l1:l2
+        })
+        console.log(largest)
+
+
+        //! Call Stack :-
+        //?     
+        
+        console.log("start")
+        function hello(){
+            console.log("hello")
+            hi();
+        }
+        function hi() {
+            console.log("im hi")
+        }
+        function good(){
+            console.log("i am good")
+        }
+
+        hello()
+        console.log("end")
